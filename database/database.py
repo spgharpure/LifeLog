d7f06c2084f26ecd40e3a1452922eb95dfb2c4e6
Shreeya Gharpure <sgharpur@uci.edu>
2026-08-24 12:37:31 -0700

    added mood table and mood database functions

--- database/database.py
+++ database/database.py
@@ -27,5 +27,10 @@ c.execute("""CREATE TABLE IF NOT EXISTS habit_checkins (
             FOREIGN KEY (habit_id) REFERENCES habits (habit_id)
             )""")
 
+c.execute("""CREATE TABLE IF NOT EXISTS moods (
+            mood_id INTEGER NOT NULL,
+            mood_date TEXT NOT NULL UNIQUE,
+            mood_value TEXT NOT NULL)""")
+
 conn.commit()
 
---
+++ database/moods_db.py
@@ -0,0 +1,40 @@
+import database.database as database
+
+def set_mood(mood_date, mood_value):
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute("SELCT mood_id from moods WHERE mood_date = ?", (mood_date,))
+    existing = c.fetchone()
+
+    if existing:
+        c.execute("UPDATE moods SET mood_value = ? WHERE mood_date = ?",
+                  (mood_value, mood_date))
+    else:
+        c.execute("INSERT INTO moods (mood_date, mood_value) VALUES (?, ?)",
+                  (mood_date, mood_value))
+
+    conn.commit()
+    conn.close()
+
+def get_all_moods():
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute("SELECT * FROM moods ORDER BY mood_date DESC")
+    rows = c.fetchall()
+
+    conn.close()
+    return rows
+
+def get_mood_for_date(mood_date):
+    conn = database.get_connection()
+    c = conn.cursor()
+
+    c.execute("SELECT mood_value FROM moods WHERE mood_date = ?", (mood_date,))
+    row = c.fetchone()
+
+    conn.close()
+    if row:
+        return row[0]
+    return None
